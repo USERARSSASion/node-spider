11697d9bfbb727490ed1f07167eb1545c7001a6c
锦龙 <majinlong@uniwyse.com>
2024-02-20 16:44:58 +0800

    add

--- 2_crawler.js
+++ 2_crawler.js
@@ -1,23 +1,35 @@
 const Crawler = require('crawler');
+// const jsdom = require("jsdom");
+// const { JSDOM } = jsdom;
 
 const c = new Crawler({
   maxConnections: 10,
-  jQuery: {
-    name: 'cheerio',
-    options: {
-        normalizeWhitespace: true,
-        xmlMode: true
-    }
-  },
+  // jQuery: {
+  //   name: 'cheerio',
+  //   options: {
+  //       normalizeWhitespace: true,
+  //       xmlMode: true
+  //   }
+  // },
+  // encoding: null,
+  // jQuery: jsdom,
+  jquery: false,
+  json: true,
+  skipDuplicates: true,
+  // retries:0,
+  // timeout:100,
   // This will be called for each crawled page
   callback: (error, res, done) => {
       if (error) {
           console.log(error);
       } else {
-          const $ = res.$;
+        console.log(res.body, res.options.platform);
+        // const { document } = (new JSDOM(res.body)).window;
+        // console.log(document.getElementById("international-home-app"));
+          // const $ = res.$;
           // $ is Cheerio by default
           //a lean implementation of core jQuery designed specifically for the server
-          console.log($('title').text());
+          // console.log($('title').text());
       }
       done();
   }
@@ -25,5 +37,6 @@ const c = new Crawler({
 
 // c.queue('http://www.amazon.com');
 // c.queue('http://www.baidu.com');
-c.queue('http://www.github.com');
-// c.queue('http://www.bilibili.com');
+// c.queue('http://www.github.com');
+// c.queue({ uri: 'http://www.bilibili.com', platform: 'bilibili' });
+c.queue({ uri: 'https://a.sina.cn/t/Inside/hotSearch/lists?is_new_engine=0&get_from=wb&sort_by=hot', platform: 'weibo' })
